6fb21acae0da597e48aa3f9c9769e311e4d01aa1
Jess Scheuring <jscheuring@gmail.com>
2016-03-30 14:47:14 -0500

    adds filter for hard coded contact list

--- mobile/components/selectContacts.js
+++ mobile/components/selectContacts.js
@@ -4,21 +4,54 @@ import React, {
   TextInput,
   Text,
   View,
+  ListView,
   TouchableOpacity
 } from 'react-native';
 
+const contacts = [
+  {name: 'jess scheuring'},
+  {name: 'nicer hugs'},
+  {name: 'bill murray'}
+]
+
 class selectContacts extends Component {
+  constructor() {
+    super();
+    var ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+    this.updateSearch = this.updateSearch.bind(this);
+    this.state = {
+      dataSource: ds.cloneWithRows([]),
+    };
+  }
   updateSearch(text) {
-    console.log(text);
+    // request contacts here
+    // when they return, update state to reflect selected ones
+    var filteredContacts = [];
+    if (text.trim()) {
+      filteredContacts = contacts.filter(contact => {
+        return contact.name.toLowerCase().indexOf(text.toLowerCase()) >= 0;
+      })
+    }
+    this.setState({dataSource: this.state.dataSource.cloneWithRows(filteredContacts)});
   }
   render() {
     return (
       <View>
         <Text>Guests</Text>
         <TextInput onChangeText={this.updateSearch}/>
+        <ListView
+          dataSource={this.state.dataSource}
+          renderRow={renderContact}
+        />
       </View>
     )
   }
+}
+
+function renderContact(rowData) {
+  return (
+    <Text>{rowData.name}</Text>
+  )
 }
 
 export default selectContacts;
