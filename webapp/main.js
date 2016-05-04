f110844e6accb65fea99d8c317d4c5d4d9a831d8
Jess Scheuring <jscheuring@gmail.com>
2016-05-04 13:53:40 -0500

    hooks up react router

--- webapp/main.js
+++ webapp/main.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import {render} from 'react-dom';
-import {Router, Route} from 'react-router';
+import {Router, Route, browserHistory} from 'react-router';
 
 import App from './components/App.js';
 
-render((<App/>), document.getElementById('app'))
+const router = (
+  <Router history={browserHistory}>
+    <Route path="/" component={App}/>
+  </Router>
+)
+
+render(router, document.getElementById('app'))
